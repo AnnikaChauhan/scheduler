fc4890eccea6fa4cc54016a559d61830f25d054e
AnnikaChauhan <annikac0711@gmail.com>
2021-05-24 17:12:22 +0100

    adds basic scheduler with ability to change view

--- src/components/ScheduleView.js
+++ src/components/ScheduleView.js
@@ -1,34 +1,45 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
-  DayView,
+  WeekView,
   Appointments,
+  Toolbar,
+  ViewSwitcher,
+  MonthView,
+  DayView,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { data } from "../data";
 
-const currentDate = "2018-11-01";
-const schedulerData = [
-  {
-    startDate: "2018-11-01T09:45",
-    endDate: "2018-11-01T11:00",
-    title: "Meeting",
-  },
-  {
-    startDate: "2018-11-01T12:00",
-    endDate: "2018-11-01T13:30",
-    title: "Go to a gym",
-  },
-];
+const View = () => {
+  const [view, setView] = useState();
+  return (
+    <Paper>
+      <Scheduler data={data}>
+        <ViewState currentDate={new Date()} />
+        <WeekView
+          startDayHour={10}
+          endDayHour={19}
+          currentViewName={view}
+          onCurrentViewNameChange={setView}
+        />
+        <WeekView
+          name="work-week"
+          displayName="Work Week"
+          excludedDays={[0, 6]}
+          startDayHour={9}
+          endDayHour={19}
+        />
+        <MonthView />
+        <DayView />
 
-const View = () => (
-  <Paper>
-    <Scheduler data={schedulerData}>
-      <ViewState currentDate={currentDate} />
-      <DayView startDayHour={9} endDayHour={14} />
-      <Appointments />
-    </Scheduler>
-  </Paper>
-);
+        <Toolbar />
+        <ViewSwitcher />
+        <Appointments />
+      </Scheduler>
+    </Paper>
+  );
+};
 
 export default View;
